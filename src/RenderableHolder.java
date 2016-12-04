c3af7ce58ea37f0c4f79c3bf64d520cb3cdd102c
naisk133 <nai_sk133@hotmail.com>
2016-12-04 12:20:22 +0700

    implement RenderableHolder singleton

---
+++ src/RenderableHolder.java
@@ -0,0 +1,49 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import com.sun.xml.internal.ws.api.model.wsdl.editable.EditableWSDLService;
+
+public class RenderableHolder {
+	public static RenderableHolder instance = new RenderableHolder();
+
+	private List<IRenderable> entities;
+	private int maxZ;
+	private Comparator<IRenderable> comparator;
+
+	private RenderableHolder() {
+		entities = new ArrayList<>();
+		maxZ = 0;
+		comparator = (IRenderable x, IRenderable y) -> {
+			if (x.getZ() > y.getZ())
+				return 1;
+			else if (x.getZ() == y.getZ())
+				return 0;
+			else
+				return -1;
+		};
+	}
+
+	public void addEntity(IRenderable renderable) {
+		entities.add(renderable);
+		sortEntity();
+	}
+
+	public void removeEntity(IRenderable renderable) {
+		entities.remove(renderable);
+		sortEntity();
+	}
+
+	public void removeEntity(int index) {
+		entities.remove(index);
+		sortEntity();
+	}
+
+	public void sortEntity() {
+		Collections.sort(entities,comparator);
+		maxZ = entities.get(entities.size() - 1).getZ();
+	}
+
+}
