c05bc34458b7553b9acdf5117f2cfc0076c89bc8
naisk133 <nai_sk133@hotmail.com>
2016-12-04 11:23:51 +0700

    add getter setter for entity

--- src/Entity.java
+++ src/Entity.java
@@ -1,10 +1,58 @@
 
 public abstract class Entity implements IRenderable {
-	
+
 	public static double GRAVITY = 9.8;
-	
+
 	protected double x, y, speedX, speedY;
 	protected int z;
 	protected boolean isDestroyed;
-	
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public double getSpeedX() {
+		return speedX;
+	}
+
+	public void setSpeedX(double speedX) {
+		this.speedX = speedX;
+	}
+
+	public double getSpeedY() {
+		return speedY;
+	}
+
+	public void setSpeedY(double speedY) {
+		this.speedY = speedY;
+	}
+
+	public int getZ() {
+		return z;
+	}
+
+	public void setZ(int z) {
+		this.z = z;
+	}
+
+	public boolean isDestroyed() {
+		return isDestroyed;
+	}
+
+	public void setDestroyed(boolean isDestroyed) {
+		this.isDestroyed = isDestroyed;
+	}
+
 }
