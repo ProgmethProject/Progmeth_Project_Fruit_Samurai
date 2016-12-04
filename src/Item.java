61022d568f07bbd97da8dc439c28031ca8b62017
Sivakorn Chanpitayanukulkij <ch.sivakorn@gmail.com>
2016-12-04 11:52:38 +0700

    Add Hazard & Item abstract class

---
+++ src/Item.java
@@ -0,0 +1,8 @@
+
+public abstract class Item extends Entity implements Cuttable, IRenderable {
+
+	public Item(double x, double y, double speedX, double speedY, int z) {
+		super(x, y, speedX, speedY, z);
+	}
+
+}
