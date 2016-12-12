d9a191e8c95e9c8b5640f40f706a54250dfd5b08
Sivakorn Chanpitayanukulkij <ch.sivakorn@gmail.com>
2016-12-12 14:38:48 +0700

    Add color selection

--- src/gui/CollectionScreen.java
+++ src/gui/CollectionScreen.java
@@ -1,8 +1,13 @@
 package gui;
 
+import Utility.DrawingUtility;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
@@ -45,7 +50,8 @@ public class CollectionScreen extends BorderPane {
 			setStyle("-fx-background-color: transparent; -fx-padding: 30");
 			
 			selectColor = new Label("Blade Color");
-			selectColor.setStyle("-fx-font-size: 32px; -fx-font-family:\"Arial Black\"; -fx-text-fill: mediumorchid;");
+			selectColor.setPrefWidth(210);
+			setLabel(selectColor, "mediumorchid");
 			
 			purpleBtn = new Button();
 			blueBtn = new Button();
@@ -65,6 +71,57 @@ public class CollectionScreen extends BorderPane {
 			add(greenBtn, 0, 2);
 			add(redBtn, 1, 2);
 			add(rainbowBtn, 0, 3);
+			
+			addListener();
+		}
+		
+		public void addListener() {
+			purpleBtn.setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent event) {
+					setLabel(selectColor, "mediumorchid");
+				}
+			});
+			
+			blueBtn.setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent event) {
+					setLabel(selectColor, "deepskyblue");
+				}
+			});
+			
+			greenBtn.setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent event) {
+					setLabel(selectColor, "limegreen");
+				}
+			});
+
+			redBtn.setOnAction(new EventHandler<ActionEvent>() {
+	
+				@Override
+				public void handle(ActionEvent event) {
+					setLabel(selectColor, "red");
+				}
+			});
+			
+			rainbowBtn.setOnAction(new EventHandler<ActionEvent>() {
+				
+				@Override
+				public void handle(ActionEvent event) {
+					setLabel(selectColor, "gray");
+				}
+			});
+			
+		}
+		
+		public void setLabel(Label label, String color) {
+			selectColor.setStyle("-fx-font-size: 30px; -fx-font-family:\"Arial Black\"; -fx-text-fill: " + color + ";"
+					+ "-fx-text-alignment:center; -fx-background-color: white; -fx-padding: 0 5 0 5;"
+					+ "-fx-border-color: black; -fx-border-width: 3;");
 		}
 		
 		public void setBtn(Button Btn, String color) {
