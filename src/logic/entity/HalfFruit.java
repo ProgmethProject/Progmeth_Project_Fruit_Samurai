9662d405cdfe7d7bee515669b678766ea56e1838
naisk133 <nai_sk133@hotmail.com>
2016-12-13 23:17:13 +0700

    remove commented codes in HalfFruit in draw()

--- src/logic/entity/HalfFruit.java
+++ src/logic/entity/HalfFruit.java
@@ -26,10 +26,6 @@ public class HalfFruit extends Entity {
 
 	@Override
 	public void draw(GraphicsContext gc) {
-//		Image image = DrawingUtility.cut_fruit[index * 2 + side];
-//
-//		DrawingUtility.drawRotatedImage(gc, image, rotation, x, y);
-//		gc.fillRect(x + image.getWidth() / 2, y + image.getHeight() / 2, 5, 5);
 		DrawingUtility.drawHalfFruit(gc, index, side, rotation, x, y);
 	}
 
